4003290c4c9b0104ff160a6edbf71ae58f5a8051
nikitaodnorob <nikitaodnorob@gmail.com>
2021-02-14 14:42:16 +0300

    moved API_HOST to .env

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,14 @@
-const REACT_APP_API_HOST = 'http://localhost:8000';
+const apiHost = process.env.REACT_APP_API_HOST;
 
 export const searchAssociates = (word) => {
-    return fetch(`${REACT_APP_API_HOST}/associate?word=${word}`).then(res => res.json());
+    return fetch(`${apiHost}/associate?word=${word}`).then(res => res.json());
 };
 
 export const paraphraseText = (text) => {
     const data = new URLSearchParams();
     data.append('text', text);
 
-    return fetch(`${REACT_APP_API_HOST}/rephrase`, {
+    return fetch(`${apiHost}/rephrase`, {
         method: 'POST',
         body: data,
     }).then(res => res.json());
