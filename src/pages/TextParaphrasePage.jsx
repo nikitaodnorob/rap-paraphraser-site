47f429cd19315f9ce3a14a9ea0bead5e0f3ad388
nikitaodnorob <nikitaodnorob@gmail.com>
2021-02-14 17:23:53 +0300

    converted class components to functional

--- src/pages/TextParaphrasePage.jsx
+++ src/pages/TextParaphrasePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Header} from '../components/Header';
 import {getTextWithReplaces} from '../helpers/replaceText';
@@ -6,60 +6,51 @@ import {ResultField} from '../components/ResultField';
 import * as api from '../api';
 import './css/TextParaphrasePage.css';
 
-export class TextParaphrasePage extends React.Component {
+export const TextParaphrasePage = () => {
+    const [isLoading, setLoading] = useState(false);
+    const [data, setData] = useState('');
+    const searchFieldRef = useRef(null);
 
-    constructor(props) {
-        super(props);
-        this.clearField = this.clearField.bind(this);
-        this.paraphraseText = this.paraphraseText.bind(this);
-        this.state = {isLoading: false, data: ''};
-    }
+    const clearField = () => {
+        searchFieldRef.current.value = '';
+        setData('');
+    };
 
-    clearField() {
-        document.querySelector('.enter-text').value = '';
-        this.setState({data: ''});
+    const paraphraseText = async () => {
+        const text = searchFieldRef.current.value;
+        setLoading(true);
 
-    }
+        const result = await api.paraphraseText(text);
+        setData(getTextWithReplaces(text, result));
+        setLoading(false);
+    };
 
-    paraphraseText() {
-        const text = document.querySelector('.enter-text').value;
-        this.setState({isLoading: true});
-        api.paraphraseText(text)
-            .then(data => this.setState({
-                data: getTextWithReplaces(text, data),
-                isLoading: false,
-            }));
-    }
-
-    render() {
-        const {data = '', isLoading} = this.state;
-        return (
-            <>
-                <Header />
-                <div className="main-div">
-                    <div>
-                        <p>Введите текст:</p>
-                        <form>
-                            <textarea name="text" className="enter-text"/>
-                            <br/>
-                            <div className="button-bar">
-                                <button type="button" onClick={this.paraphraseText}>
-                                    Отправить!
-                                </button>
-                                <button type="button" onClick={this.clearField}>Очистить</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div>
-                        <p>Измененный текст:</p>
-                        <ResultField text={data} isLoading={isLoading} baseClass="ws-pre-wrap"/>
-                        <div>
-                            <Link to="/">Перейти на стартовую страницу</Link><br/>
-                            <Link to="/associations">Перейти к поиску ассоциатов</Link>
+    return (
+        <>
+            <Header />
+            <div className="main-div">
+                <div>
+                    <p>Введите текст:</p>
+                    <form>
+                        <textarea name="text" className="enter-text" ref={searchFieldRef} />
+                        <br/>
+                        <div className="button-bar">
+                            <button type="button" onClick={paraphraseText}>
+                                Отправить!
+                            </button>
+                            <button type="button" onClick={clearField}>Очистить</button>
                         </div>
+                    </form>
+                </div>
+                <div>
+                    <p>Измененный текст:</p>
+                    <ResultField text={data} isLoading={isLoading} baseClass="ws-pre-wrap"/>
+                    <div>
+                        <Link to="/">Перейти на стартовую страницу</Link><br/>
+                        <Link to="/associations">Перейти к поиску ассоциатов</Link>
                     </div>
                 </div>
-            </>
-        );
-    }
-}
+            </div>
+        </>
+    );
+};
